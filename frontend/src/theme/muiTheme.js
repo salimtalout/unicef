7dabdde24e17cf2671608129c1ff16229d42f6d9
Colas Gabriac <colas.gabriac@sia-partners.com>
2019-09-28 12:06:26 +0200

    dashboards files

---
+++ frontend/src/theme/muiTheme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const muiTheme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    secondary: {
+      light: '#1CE8B5',
+      main: '#fff',
+      dark: '#660033',
+      contrastText: '#660033',
+    },
+    primary: {
+      light: '#1CE8B5',
+      main: '#660033',
+      dark: '#660033',
+      contrastText: '#fff',
+    }
+  },
+});
+
+export default muiTheme;
