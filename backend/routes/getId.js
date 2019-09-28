0d5dba35264259f9187e955036b5f043bf01c8d6
Salim Talout Zitan <salim.taloutzitan@sia-partners.com>
2019-09-28 16:09:40 +0200

    .

--- backend/routes/getId.js
+++ backend/routes/getId.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function (req, res, next) {
+router.post('/', function (req, res, next) {ipfs.get(hash, function (err, files) {
+  // recuperer le hash depuis le smart contract grace à req.body.uuid
   ipfs.get(hash, function (err, files) {
     files.forEach(file => {
       console.log(file);
