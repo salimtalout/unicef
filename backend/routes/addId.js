76e64d31ba6409525ce4694d4609847ce5bbf0fd
Salim Talout Zitan <salim.taloutzitan@sia-partners.com>
2019-09-28 15:30:23 +0200

    ipfs add id

--- backend/routes/addId.js
+++ backend/routes/addId.js
@@ -1,12 +1,28 @@
 var express = require('express');
-const fs = require("fs");
 const ipfsClient = require("ipfs-http-client");
 var router = express.Router();
 const ipfs = ipfsClient("localhost", "5001", { protocol: "http" });
 
 router.post('/', async function (req, res, next) {
-  const content = fs.readFileSync("test.txt", "utf8");
-  const results = await ipfs.add(content);
+  const content = {
+    uuid: req.body.uuid,
+    nom: req.body.nom,
+    prenom: req.body.prenom,
+    dateNaissance: req.body.dateNaissance,
+    lieuNaissance: req.body.lieuNaissance,
+    acteNaissance: req.body.acteNaissance,
+    fingerprint: res.body.fingerprint,
+    signatureOracle: res.body.signatureOracle,
+    commentaire: res.body.commentaire,
+    couleurYeux: res.body.couleurYeux,
+    sexe: req.body.sexe,
+    parent1: req.body.parent1,
+    parent2: req.body.parent2,
+    enfants: req.body.enfants,
+    autres: req.body.autres,
+    photo: req.body.photo
+  };
+  const results = await ipfs.add(JSON.stringify(content));
   const hash = results[0].hash;
 });
 
