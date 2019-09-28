90fc286604ecf53754e4034be4883a484be97270
Salim Talout Zitan <salim.taloutzitan@sia-partners.com>
2019-09-28 14:33:03 +0200

    .

---
+++ frontend/src/header/list.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import FileIcon from '@material-ui/icons/FileCopy';
+import ListIcon from '@material-ui/icons/List';
+
+const ItemList = (
+  <div>
+    <ListItem button onClick={() => window.location.href = "/identity/create"}>
+      <ListItemIcon>
+        <FileIcon />
+      </ListItemIcon>
+      <ListItemText style={{ marginLeft: -25 }} primary="Enregistrer une identité" />
+    </ListItem>
+    <ListItem button onClick={() => window.location.href = "/identity/query"}>
+      <ListItemIcon>
+        <ListIcon />
+      </ListItemIcon>
+      <ListItemText style={{ marginLeft: -25 }} primary="Accéder à une identité" />
+    </ListItem>
+    <ListItem button onClick={() => window.location.href = "/reporting"}>
+      <ListItemIcon>
+        <ListIcon />
+      </ListItemIcon>
+      <ListItemText style={{ marginLeft: -25 }} primary="Reporting des identités" />
+    </ListItem>
+    <ListItem button onClick={() => window.location.href = "/autority/add"}>
+      <ListItemIcon>
+        <ListIcon />
+      </ListItemIcon>
+      <ListItemText style={{ marginLeft: -25 }} primary="Gérer les droits" />
+    </ListItem>
+  </div>
+);
+
+export default ItemList
